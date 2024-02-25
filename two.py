69ab2cba0b3c27ef208c71d7c80b5734746bff47
Danny Wong Jia Hong <danny-wong-02@hotmail.com>
2024-02-25 17:05:26 +0800

    refactor two.py

--- two.py
+++ two.py
@@ -78,7 +78,6 @@ class two(tk.Frame):
     third_url = driver.current_url
     print("Third URL:", third_url)
 
-
     time.sleep(3)
     # Selecting the multi-select element by locating its id
     select = Select(driver.find_element(By.ID,"ctl00_cntPlcHldrContent_selEODEnquiry"))
@@ -130,9 +129,6 @@ class two(tk.Frame):
     # Get the HTML content of the fifth URL
     html_content_fifth_url = driver.page_source
 
-    # Print the HTML content of the fifth URL
-    # print("HTML Content of Fifth URL:", html_content_fifth_url)
-
     # Parse the HTML
     soup = BeautifulSoup(html_content_fifth_url, "html.parser")
 
@@ -148,41 +144,8 @@ class two(tk.Frame):
     # Print the extracted data
     print(process_date_label + ":", process_date_value)
 
-    # Extract data from the first table
-    data1 = []
-    for row in table1.find_all("td", id="tdBG"):
-        row_data = []
-        for cell in row.find_all(["span"]):
-            cell_text = cell.get_text(strip=True)
-            if cell_text:  # Check if cell text is not empty
-                row_data.append(cell_text)
-        if row_data:  # Only append if row_data is not empty
-            data1.append(row_data)
-
-    # Extract table headers from the second table
-    table2_headers = []
-    for th in table2.find_all("th"):
-        header_text = th.get_text(strip=True)
-        if header_text:  # Check if header text is not empty
-            table2_headers.append(header_text)
-
-    # Extract data from the second table
-    data2 = []
-    for row in table2.find_all("tr"):
-        row_data = []
-        # Flag to check if any non-empty cell is found in the row
-        non_empty_found = False
-        for cell in row.find_all("td"):
-            cell_text = cell.get_text(strip=True)
-            if cell_text:  # If cell text is not empty
-                non_empty_found = True
-            row_data.append(cell_text)
-        # Append the row data only if at least one non-empty cell is found
-        if non_empty_found:
-            data2.append(row_data)
-
-    # Combine data from table1 and table2
-    combined_data = data1 + [table2_headers] + data2 
+    # Extract data from the BTX table
+    data1, combined_data = extract_table_data(table1, table2)
 
     # Create a folder named "xlsx_files" if it doesn't exist
     folder_name = "xlsx_files"
@@ -200,55 +163,7 @@ class two(tk.Frame):
     # Path to the Excel file inside the subfolder
     excel_file_path = os.path.join(subfolder_name, "tableTwo.xlsx")
 
-    # Create a new workbook and add a worksheet
-    workbook = xlsxwriter.Workbook(excel_file_path)
-    worksheet = workbook.add_worksheet('Sheet1')
-
-    # Define colors in hexadecimal format
-    white_color_hex = '#FFFFFF'
-    grey_color_hex = '#f5f5f5'
-    deep_grey_color_hex = '#d7dae1'
-    blue_font_hex = '#0b14fe'
-
-    # Define bold format
-    bold_format = workbook.add_format({'bold': True})
-
-    # Write combined data to XLSX
-    for row_index, row_data in enumerate(combined_data):
-        # Choose color based on row index
-        if row_index == len(data1):  # Header row
-            cell_format = workbook.add_format({'bg_color': deep_grey_color_hex, 'font_color': blue_font_hex})
-        elif row_index % 2 == 0:
-            cell_format = workbook.add_format({'bg_color': white_color_hex})  # White color
-        else:
-            cell_format = workbook.add_format({'bg_color': grey_color_hex})  # Grey color
-
-        for col_index, cell_data in enumerate(row_data):
-            if row_index == len(data1):  # Header row
-                if cell_data == "Task ID":  # Adjust width for "Task Name" header
-                    worksheet.set_column(col_index, col_index, 25)  # Set width to 300px (approx.)
-                elif cell_data == "Task Name":
-                    worksheet.set_column(col_index, col_index, 40)  # Set width for "Start Time" header
-                elif cell_data == "Start Time":
-                    worksheet.set_column(col_index, col_index, 20)  # Set width for "Start Time" header
-                elif cell_data == "Actual Start Time":
-                    worksheet.set_column(col_index, col_index, 20)  # Set width for "Actual Start Time" header
-                elif cell_data == "Actual End Time":
-                    worksheet.set_column(col_index, col_index, 20)  # Set width for "Actual End Time" header
-                elif cell_data == "Duration":
-                    worksheet.set_column(col_index, col_index, 20)  # Set width for "Duration" header
-                elif cell_data == "Status":
-                    worksheet.set_column(col_index, col_index, 25)  # Set width for "Status" header
-                worksheet.write(row_index, col_index, cell_data, bold_format)
-            else:
-                worksheet.write(row_index, col_index, cell_data, cell_format)
-
-    # Adjust column widths
-    for i, column in enumerate(zip(*combined_data)):
-        max_length = max(len(str(cell)) for cell in column)
-        worksheet.set_column(i, i, max_length +5)  # Add a little extra space
-
-    workbook.close()
+    generate_excel_file(excel_file_path, combined_data, data1)
 
     # Load the Excel file
     file_path = excel_file_path
@@ -263,42 +178,11 @@ class two(tk.Frame):
     # Convert the DataFrame to an HTML table with no index
     html_table = df.to_html(index=False, header=False)
 
-    # Modify the HTML table to make the header grey
-    soup = BeautifulSoup(html_table, "html.parser")
-    tr_elements = soup.find_all("tr")[1:]  # Select rows starting from the third row
-
-    for idx, element in enumerate(soup.find_all(["td"])):
-        if element.name == "td":
-            if element.text.strip() in ["Task ID", "Task Name", "Start Time", "Actual Start Time", "Actual End Time", "Duration", "Next Day", "Status"]:
-                element['style'] = 'background-color:#d7dae1; color:#0b14fe; font-weight:bold;'
-
-    # Iterate over each table row, starting from the second row
-    for tr_element in tr_elements[0:]:
-        # Find all table cells within the current row
-        td_elements = tr_element.find_all("td")
-        
-        # Iterate over each table cell within the current row
-        for td_element in td_elements:
-            # Add padding to the cell
-            td_element['style'] = 'padding: 5px;'
-        
-        # Get the last table cell in the current row
-        last_td_element = td_elements[-1]
-        
-        # Check if the text content of the last cell contains "Process Succeeded!"
-        if "Process Succeeded!" in last_td_element.text.strip():
-            # If it does, set the background color to green and text color to white
-            last_td_element['style'] += 'background-color: green; color: white;'
-        else:
-            # If not, set the background color to red and text color to white
-            last_td_element['style'] += 'background-color: red; color: white;'
-
-    html_table = str(soup)
+    modified_html_table = modify_html_table(html_table)
 
     body = generate_email_body(image1_base64,image2_base64)
 
-    # html_content = f"<p>Process Date: {process_date_value}</p>\n" + html_table + body
-    html_content = f"<p>Process Date: {process_date_value}</p>\n\n{html_table}\n{body}"
+    html_content = f"<p>Process Date: {process_date_value}</p>\n\n{modified_html_table}\n{body}"
     # Set up the email details
     sender_email = "dannywong@kenanga.com.my"
     receiver_email = ["dannywong@kenanga.com.my"]
@@ -339,6 +223,7 @@ class two(tk.Frame):
     # Switch back to the main window
     driver.switch_to.window(driver.window_handles[0])
     time.sleep(3)
+
     #Perform Logout Operation
     logoutButton = driver.find_element(By.ID, "ctl00_lbtnLogout")
     # logoutButton = driver.find_element(By.XPATH, "//table[@id='tblHeader']/tbody/tr/td/table/tbody/tr/td[8]/a[@id='lbNext']")
