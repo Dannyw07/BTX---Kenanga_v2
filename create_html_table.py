30a0e1aa07353b3a47fb28fb5a2b93b81656cb66
Danny Wong Jia Hong <danny-wong-02@hotmail.com>
2024-03-08 10:07:04 +0800

    add process started detection

--- create_html_table.py
+++ create_html_table.py
@@ -30,6 +30,9 @@ def modify_html_table(html_table):
         if "Process Succeeded!" in last_td_element.text.strip():
             # If it does, set the background color to green and text color to white
             last_td_element['style'] += 'background-color: green; color: white;'
+        elif "Process started" in last_td_element.text.strip():
+            # If it contains "Process started", set the background color to yellow and text color to black
+            last_td_element['style'] += 'background-color: yellow; color: white;'
         else:
             # If not, set the background color to red and text color to white
             last_td_element['style'] += 'background-color: red; color: white;'
